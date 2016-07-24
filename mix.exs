9de97eaacf8ebe4f1281ccb9f1d261f72df195cb
Bartosz Nowak <sztosz@gmail.com>
2016-07-24 12:05:45 +0200

    Videos

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Rumbl.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.2.0"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_ecto, "~> 2.0"},
+     {:phoenix_ecto, "~> 3.0"},
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
